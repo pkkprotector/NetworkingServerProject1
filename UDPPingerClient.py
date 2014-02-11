3bf76606913f1c3890a75de4ea091c71521038bd
Jonathan Thornton <pkkprotector@gmailc.om>
2014-02-11 16:53:30 -0500

    Added in Optional Exercises
    Checks all pings not just the accepted ones then times out.

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -9,29 +9,47 @@
 #UDPPingerClient.py
 
 from socket import *
-import time
-serverName = 'localhost'
-serverPort = 12005
+from datetime import datetime
+serverName = '10.250.13.27'
+serverPort = 12000
 clientSocket = socket(AF_INET,SOCK_DGRAM)
 message = 'Ping'
-counter = 10 			#total number of pings we send to the server
-i = 0 					#this will be our counted pings.
+counter = 10                    #total number of pings we send to the server
+i = 0                                   #this will be our counted pings.
 leftover = counter - i
+lowestping = 10000000000 #this will be our lowest ping 
+highestping = 0 #this will be the max number
+pingpercent = 0
+averageping = 0
+acceptedpings = 0
 print 'Sending '+ `counter` + 'to server \n'
-try:
-        while i < counter:
-                i+= 1
-                print '\n Ping attempt number '+`i`+' is currently in progress. \n'
-                print 'Number of pings left: ' + `leftover`
-                dt= time.clock()
-                clientSocket.sendto(message,(serverName,serverPort))
-                clientSocket.settimeout(1)
+while i < counter:
+        i+= 1
+        print '\n Ping attempt number '+`i`+' is currently in progress. \n'
+        print 'Number of pings left: ' + `leftover`
+        start= datetime.now()
+        leftover = counter - i
+        clientSocket.sendto(message,(serverName,serverPort))
+        clientSocket.settimeout(1)
+        try:
                 modifiedMessage,serverAddress = clientSocket.recvfrom(1024)
-                dt2 = time.clock()
-                et = dt2 - dt;
+                final = datetime.now()
+                totaltime = start - final;
+                averageping = averageping + totaltime.microseconds
+                acceptedpings = acceptedpings+1
+                if(totaltime.microseconds < lowestping):
+                        lowestping = totaltime.microseconds
+                if(totaltime.microseconds > highestping):
+                        highestping = totaltime.microseconds
                 print modifiedMessage
-                print 'Time elapsed '+`et`+ ' microseconds. \n'
-except timeout:
-	print 'Request timed out'
+                print 'Time elapsed ',totaltime.microseconds,' microseconds. \n'
+        except timeout:
+                print 'Request timed out'
 if i == 10:
-	clientSocket.close()
+        finalaverage = averageping/acceptedpings
+        print 'Highest ping time was: ', highestping,'\n'
+        print 'Lowest ping time was: ', lowestping,'\n'
+        print 'Average ping time was: ', finalaverage, '\n'
+        pingpercent = 1- acceptedpings/float(i)
+        print 'Percent loss was: ',pingpercent,'% \n'
+        clientSocket.close()
